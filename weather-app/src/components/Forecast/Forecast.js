8505c3f7a2c71fd483d156f40c1bcc88ac6b29b0
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-06-17 12:33:58 +0200

    Forecast.js : added comments

--- weather-app/src/components/Forecast/Forecast.js
+++ weather-app/src/components/Forecast/Forecast.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Forecast = () => {
+
+    // function with fetch
     function getForecast() {
         fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Madrid&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
             "method": "GET",
@@ -16,6 +18,8 @@ const Forecast = () => {
         });
     }
     return (
+
+        // brackets allows mix JavaScript with HTML
         <div>
            <h2>Find Current Weather Conditions Right Now</h2>
            <div>
