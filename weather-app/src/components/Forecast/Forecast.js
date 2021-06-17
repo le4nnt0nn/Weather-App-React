5e44a6f154d2c82abd577081524eba2d9258d69d
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-06-17 12:27:26 +0200

    created Forecast.js

---
+++ weather-app/src/components/Forecast/Forecast.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Forecast = () => {
+    function getForecast() {
+       // weather data fetch 
+    }
+    return (
+        
+    )
+}
+
+ export default Forecast;
