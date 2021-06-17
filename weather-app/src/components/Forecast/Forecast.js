e70d85d7d52da8f2828f1b48bed0d2e6a9b882a8
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-06-17 12:31:06 +0200

    Forecast.js : added fetch to getForecast and return div

--- weather-app/src/components/Forecast/Forecast.js
+++ weather-app/src/components/Forecast/Forecast.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 const Forecast = () => {
     function getForecast() {
-       // weather data fetch 
+        fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Madrid&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+            }
+        })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.error(err);
+        });
     }
     return (
-        
+        <div>
+           <h2>Find Current Weather Conditions Right Now</h2>
+           <div>
+               {JSON.stringify(responseObj)}
+           </div>
+           <button onClick={getForecast}>Get Forecast</button>
+       </div>
     )
 }
 
