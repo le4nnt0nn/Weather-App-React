5fa7c7eddaa51260152cb28429d2fe005efa0e2f
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-06-20 18:08:48 +0200

    Forecast.js : fetch changed

--- weather-app/src/components/Forecast/Forecast.js
+++ weather-app/src/components/Forecast/Forecast.js
@@ -5,25 +5,20 @@ const Forecast = () => {
 
   // function with fetch
   function getForecast() {
-    fetch(
-      "https://community-open-weather-map.p.rapidapi.com/weather?q=madrid%2Ces&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html",
-      {
-        method: "GET",
-        headers: {
-          "x-rapidapi-key":
-            "5d42d2897emsh6345ce50d7b7adcp1ca28cjsnee0a752867ba",
-          "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-        },
-      }
-    )
-    // response to jsonObject
-    .then(response => response.json())
-    .then(response => {
-        setResponseObj(response)
-    })
-      .catch((err) => {
-        console.error(err);
-      });
+    fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=madrid%2Ces&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "5d42d2897emsh6345ce50d7b7adcp1ca28cjsnee0a752867ba",
+		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+	}
+})
+.then(response => response.json())
+.then(response => {
+    setResponseObj(response)
+})
+.catch(err => {
+	console.error(err);
+});
   }
   return (
     // brackets allows mix JavaScript with HTML
