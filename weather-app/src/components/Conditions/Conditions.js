93db92800023fb55b2a9a50e05c4e018af17d891
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-06-22 12:03:48 +0200

    Conditions.js : created const with props and if

---
+++ weather-app/src/components/Conditions/Conditions.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Conditions = (props) => {
+    //if response code is 200, then return city, temp and weather. If not, return null
+   return (
+       <div>
+           {props.responseObj.cod === 200 ?
+               <div>
+                   <p><strong>{props.responseObj.name}</strong></p>
+                   <p>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
+               </div>
+           : null
+           }
+       </div>
+   )
+}
+
+export default Conditions;
