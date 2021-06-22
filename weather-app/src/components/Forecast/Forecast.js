8a3a1ca26c043d4dc1d2187bb51027f53a58a32b
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-06-22 12:15:04 +0200

    Forecast.js : let to city and unit (custom url)

--- weather-app/src/components/Forecast/Forecast.js
+++ weather-app/src/components/Forecast/Forecast.js
@@ -4,9 +4,15 @@ import Conditions from "../Conditions/Conditions";
 const Forecast = () => {
   let [responseObj, setResponseObj] = useState({});
 
+  let [city, setCity] = useState('');
+  let [unit, setUnit] = useState('metric');
+
+
+  const uriEncodedCity = encodeURIComponent(city);
+
   // function with fetch
   function getForecast() {
-    fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=madrid", {
+    fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "5d42d2897emsh6345ce50d7b7adcp1ca28cjsnee0a752867ba",
