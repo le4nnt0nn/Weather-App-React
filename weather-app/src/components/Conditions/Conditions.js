25a3351667ff2e2cd382a1ae07b0faa0fe5c3d43
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-06-28 12:08:38 +0200

    Conditions.js changed error and loading props

--- weather-app/src/components/Conditions/Conditions.js
+++ weather-app/src/components/Conditions/Conditions.js
@@ -5,10 +5,12 @@ const Conditions = (props) => {
     //if response code is 200, then return city, temp and weather. If not, return null
    return (
        <div className={classes.Wrapper}>
+
+            {props.error && <small className={classes.Small}>Please insert a valid city.</small>}
+            {props.loading && <div className={classes.Loader} />}
+
            {props.responseObj.cod === 200 ?
                <div>
-                   {props.error && <small>Please insert valid city.</small>}
-                   {props.loading && <div>Loading...</div>}
                    <p><strong>{props.responseObj.name}</strong></p>
                    <p>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
                </div>
